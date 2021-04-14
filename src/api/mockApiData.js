1142b04d5a0eec92126617949e4bfe9132a64c30
Drew Ross <mail.drew.ross@gmail.com>
2021-04-14 11:13:19 -0700

    add mockApiData file and mockGetChildTasks data

---
+++ src/api/mockApiData.js
@@ -0,0 +1,16 @@
+const mockGetChildTasks = {
+  ID: 1,
+  ChildID: 1,
+  StoryID: 1,
+  HasRead: true,
+  HasWritten: true,
+  HasDrawn: true,
+  Complexity: 30,
+  LowConfidence: false,
+  Status: 'PENDING',
+  CohortID: 1,
+};
+
+module.exports = {
+  mockGetChildTasks,
+};
