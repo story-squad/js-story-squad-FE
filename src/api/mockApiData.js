1f99c1f7df21e949762ddec6387ebddc0639fd22
Drew Ross <mail.drew.ross@gmail.com>
2021-04-14 12:15:20 -0700

    add mock api data

--- src/api/mockApiData.js
+++ src/api/mockApiData.js
@@ -3,7 +3,7 @@ const mockGetChildTasks = {
   ChildID: 1,
   StoryID: 1,
   HasRead: true,
-  HasWritten: true,
+  HasWritten: false,
   HasDrawn: true,
   Complexity: 30,
   LowConfidence: false,
@@ -11,6 +11,25 @@ const mockGetChildTasks = {
   CohortID: 1,
 };
 
+const mockNewDrawingSub = [
+  {
+    URL: 'http://someurl.com',
+    checksum:
+      '25ef9314704f5f68b7e04513c1ca13c9146328ee14a38e1d7c99789ab11fae31e1c0238425d58581b3ac4941884cd389b7bea3f8e658f533adc7cf934bb130f8',
+  },
+];
+
+const mockNewWritingSub = [
+  {
+    URL: 'http://someurl.com',
+    checksum:
+      '25ef9314704f5f68b7e04513c1ca13c9146328ee14a38e1d7c99789ab11fae31e1c0238425d58581b3ac4941884cd389b7bea3f8e658f533adc7cf934bb130f8',
+    PageNum: 1,
+  },
+];
+
 module.exports = {
   mockGetChildTasks,
+  mockNewWritingSub,
+  mockNewDrawingSub,
 };
