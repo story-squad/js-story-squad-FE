869eace7294cd1222f6b15b171c8f5a6ce355976
Drew <mail.drew.ross@gmail.com>
2021-04-22 17:27:01 -0700

    remove unused code in RenderChildDashboard

--- src/components/pages/ChildDashboard/RenderChildDashboard.js
+++ src/components/pages/ChildDashboard/RenderChildDashboard.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react';
-import { Row, Col } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { InstructionsModal } from '../../common';
 import { modalInstructions } from '../../../utils/helpers';
-
-import adventure_passport from '../../../assets/images/child_dashboard_images/adventure_passport.svg';
-import change_your_avatar from '../../../assets/images/child_dashboard_images/change_your_avatar.svg';
-import trophy_room from '../../../assets/images/child_dashboard_images/trophy_room.svg';
 
 const RenderChildDashboard = props => {
   const { push } = useHistory();
   const [modalVisible, setModalVisible] = useState(false);
 
-  const handleAcceptMission = e => {
+  const handleAcceptMission = () => {
     push('/child/mission-control');
-  };
-
-  const handleJoinSquad = e => {
-    push('/child/join');
-  };
-
-  const handleAdminPage = event => {
-    push('/admin');
-  };
-
-  const handleTrophyRoom = e => {
-    push('/child/trophyroom');
   };
 
   return (
