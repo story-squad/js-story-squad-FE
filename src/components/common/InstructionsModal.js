7a2d5c97f9edaf0dc3408bfaa59747fceaee1ec1
Drew <mail.drew.ross@gmail.com>
2021-04-26 14:18:29 -0700

    refactor, simplify InstructionsModal

--- src/components/common/InstructionsModal.js
+++ src/components/common/InstructionsModal.js
@@ -1,44 +1,23 @@
 import React from 'react';
-import { Modal, Button } from 'antd';
-// import { useHistory } from 'react-router-dom';
 
 const InstructionsModal = props => {
-  // const {push} = useHistory();
-  const {
-    modalVisible,
-    style,
-    instructions,
-    showOkButton = false,
-    handleCancel,
-    handleOk,
-  } = props;
-
-  // For when we're ready for checking the status of the weekly game
-  // const joinSquad = e => {
-  //   push('/child/join');
-  // };
+  const { visible, instructions, header, handleOk } = props;
 
   return (
     <>
-      <Modal
-        className="instructions-modal"
-        visible={modalVisible}
-        keyboard={true}
-        width={'70%'}
-        onCancel={handleCancel}
-        zIndex={2000}
-        onOk={handleOk}
-        cancelButtonProps={{ disabled: true }}
-        footer={null}
-        closeIcon={null}
-      >
-        <p style={style}>{instructions}</p>
-        {showOkButton && (
-          <Button onClick={handleCancel} className="accept-button">
-            I Accept!!!
-          </Button>
-        )}
-      </Modal>
+      {visible ? (
+        <div className="modal-container">
+          <div className="shaped-shadow-container">
+            <section className="instructions-modal content-box shaped bg-aqua">
+              <h2>{header}</h2>
+              <p>{instructions}</p>
+              <button className="h3" onClick={handleOk}>
+                Continue
+              </button>
+            </section>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
