7d884576e33ce737127efa98a0c86ad38188c578
Drew <mail.drew.ross@gmail.com>
2021-04-26 11:45:13 -0700

    refactor for styling and remove unused code

--- src/components/pages/WritingSub/RenderWritingSub.js
+++ src/components/pages/WritingSub/RenderWritingSub.js
@@ -1,70 +1,38 @@
-import React, { useState } from 'react';
-import { Row } from 'antd';
+import React from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 import { UploadDocs } from '../../common/';
 import { postNewWritingSub } from '../../../api/index';
-import { InstructionsModal } from '../../common/index';
-import { modalInstructions } from '../../../utils/helpers';
 import { tasks } from '../../../state/actions';
 
 export const RenderWritingSub = props => {
-  //Modal state
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalText, setModalText] = useState('');
-
-  const { push } = useHistory();
-
-  const handleSubmit = () => {
-    setModalVisible(true);
-    setModalText(modalInstructions.submissionComplete);
-  };
+  // TODO add event to occur after submit if needed, or remove
+  const handleSubmit = () => null;
 
   return (
-    <>
-      <InstructionsModal
-        modalVisible={modalVisible}
-        handleCancel={() => {
-          setModalVisible(false);
-          if (props.tasks.hasWritten) {
-            push('/child/mission-control');
-          }
-        }}
-        handleOk={() => {
-          setModalVisible(false);
-        }}
-        instructions={modalText}
-        style={{ fontSize: '1.5rem' }}
-      />
-      <div className="writing-sub-container">
-        <Row className="main-row">
-          {/* This will be the topic the user needs to write about. The dummy writingPrompt data in the seeds are just place holders for now and need to get updated */}
-          <p>
-            <span>Prompt: </span>
-            {props.tasks.story.writingPrompt}
-          </p>
-        </Row>
-        <p className="writing-sub-instruction">
-          {modalInstructions.writingSub}
+    <div className="content-box-mission bg-yellow">
+      {/* left */}
+      <div></div>
+      {/* right */}
+      <div>
+        <h2 className="text-align-left">Pencils Ready!</h2>
+        <p className="small">
+          <span>Prompt: </span>
+          {props.tasks.story.drawingPrompt}
         </p>
-        <div className="upload">
-          <h1>Upload</h1>
-          <UploadDocs
-            submitButtonClassname="orange-submit-button"
-            uploadButtonText="Choose files from your device"
-            uploadButtonClassname="uploadButton"
-            fileName="pages"
-            apiAxios={postNewWritingSub}
-            submissionId={props.tasks.id}
-            storyId={props.tasks.story_id}
-            setSubmitted={props.setHasWritten}
-            maxLength={5}
-            handleSubmit={handleSubmit}
-          />
-        </div>
+        <UploadDocs
+          uploadButtonText="Upload Story"
+          uploadButtonClassname="uploadButton"
+          fileName="pages"
+          apiAxios={postNewWritingSub}
+          submissionId={props.tasks.id}
+          storyId={props.tasks.story_id}
+          setSubmitted={props.setHasWritten}
+          maxLength={5}
+          handleSubmit={handleSubmit}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
