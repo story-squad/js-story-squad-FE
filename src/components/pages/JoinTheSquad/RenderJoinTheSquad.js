ffe4af37e6c0eee0cc4cf5d05d5a0239e709d1c9
Drew <mail.drew.ross@gmail.com>
2021-04-27 13:26:04 -0700

    update div className, add nav button

--- src/components/pages/JoinTheSquad/RenderJoinTheSquad.js
+++ src/components/pages/JoinTheSquad/RenderJoinTheSquad.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import RenderChild from './RenderChild';
 
 const RenderJoinTheSquad = props => {
+  const { push } = useHistory();
+
   return (
     <div className="bg-dark full-page">
-      <div className="content-box bg-dark border-light">
+      <div className="content-box dark border-light">
         <RenderChild
           child={props.team.child1}
           childNum={1}
@@ -15,6 +18,9 @@ const RenderJoinTheSquad = props => {
           childNum={2}
           bubbleStyle={'left'}
         />
+      </div>
+      <div className="center-content">
+        <button onClick={() => push('/child/point-share')}>Let's play!</button>
       </div>
     </div>
   );
