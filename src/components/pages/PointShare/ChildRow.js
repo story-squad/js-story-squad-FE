ef13cc4ed143aac4749622cdf26b9f916a86b954
Drew <mail.drew.ross@gmail.com>
2021-04-28 13:52:12 -0700

    refactor for styling

--- src/components/pages/PointShare/ChildRow.js
+++ src/components/pages/PointShare/ChildRow.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Row, Col } from 'antd';
 import Avatar from './Avatar';
-import SubmissionModal from './SubmissionModal';
+import PointShareSubmission from './PointShareSubmission';
 
 const ChildRow = ({
   child,
@@ -12,52 +11,28 @@ const ChildRow = ({
   openModal,
 }) => {
   return (
-    <Row className="portfolio-row h-100 w-100 d-flex">
-      <Col
-        className="avatar-container w-100 h-100 d-flex justify-center align-center"
-        xl={4}
-        lg={4}
-        md={24}
-        sm={24}
-        xs={24}
-      >
+    <div className="point-share-child-row content-box dark">
+      <div className="avatar-container">
         <Avatar avatarUrl={child.AvatarURL} />
-      </Col>
-      <Col
-        lg={20}
-        md={24}
-        className={`background-${bgVariable} submissions-row d-flex justify-center align-center w-100`}
-      >
-        <Row className="d-flex justify-space-around align-center w-100">
-          <Col
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            className="d-flex justify-center align-centers"
-          >
-            <SubmissionModal
-              updatePoints={updatePoints}
-              imgUrl={child.ImgURL}
-              points={points}
-              openModal={openModal}
-              submissionType={'illustration'}
-              childNum={childNum}
-            />
-          </Col>
-          <Col lg={12} md={12} sm={24} xs={24}>
-            <SubmissionModal
-              updatePoints={updatePoints}
-              imgUrl={child.Pages[0].PageURL}
-              points={points}
-              openModal={openModal}
-              submissionType={'story'}
-              childNum={childNum}
-            />
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+        <p className="text-light font-display">{child.ChildName}</p>
+      </div>
+      <PointShareSubmission
+        updatePoints={updatePoints}
+        imgUrl={child.ImgURL}
+        points={points}
+        openModal={openModal}
+        submissionType={'illustration'}
+        childNum={childNum}
+      />
+      <PointShareSubmission
+        updatePoints={updatePoints}
+        imgUrl={child.Pages[0].PageURL}
+        points={points}
+        openModal={openModal}
+        submissionType={'story'}
+        childNum={childNum}
+      />
+    </div>
   );
 };
 
