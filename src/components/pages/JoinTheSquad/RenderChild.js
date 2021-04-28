1594840af46849030d5b20acc4039a8da6f0282f
Drew <mail.drew.ross@gmail.com>
2021-04-28 09:56:23 -0700

    remove unneded buttons, code

--- src/components/pages/JoinTheSquad/RenderChild.js
+++ src/components/pages/JoinTheSquad/RenderChild.js
@@ -1,73 +1,32 @@
 import React from 'react';
-import { Col, Button } from 'antd';
 import Squadup from '../../../assets/images/Squadup.svg';
 import wordBubble from '../../../assets/images/match_up_images/wordbubble.svg';
 import wordBubbleright from '../../../assets/images/match_up_images/wordBubbleright.svg';
-import { useHistory } from 'react-router-dom';
-
-const useStyles = childNum => {
-  return {
-    joinSquad: childNum === 1 ? 'joinSquad1' : 'joinSquad2',
-    imgContain: childNum === 1 ? 'imgContain1' : 'imgContain2',
-    text: 'text',
-    wordBubble: childNum === 1 ? 'wordBubble' : 'wordBubble2',
-    star: 'star',
-    childAvatar: childNum === 1 ? 'child1-avatar' : 'child2-avatar',
-    button: 'back-button',
-  };
-};
 
 const RenderChild = ({ child, bubbleStyle, childNum }) => {
-  const { push } = useHistory();
-  const classes = useStyles(childNum);
-
-  const home = e => {
-    push('/child/dashboard');
-  };
-  const teamVote = e => {
-    push('/child/point-share');
-  };
-
   return (
-    <Col className={classes.joinSquad} xs={24} sm={12}>
-      <div className={classes.imgContain}>
-        <p className={classes.text}>
-          Hi! <br></br>My name is {child.ChildName}!
-        </p>
-        <img
-          className={classes.wordBubble}
-          src={bubbleStyle === 'right' ? wordBubbleright : wordBubble}
-          alt="word bubble"
-        />
-        <img
-          className={classes.star}
-          src={Squadup}
-          alt="Blast Character Background"
-        />
-        <img
-          className={classes.childAvatar}
-          src={child.AvatarURL}
-          alt="Child 1 Avatar"
-        />
-      </div>
-      <div className="button">
-        {childNum === 1 ? (
-          <Button className={classes.button} onClick={home}>
-            Back
-          </Button>
-        ) : (
-          <Button
-            selection="#eb7d5bbb"
-            className="sharePoints"
-            type="primary"
-            size="large"
-            onClick={teamVote}
-          >
-            Share Points
-          </Button>
-        )}
-      </div>
-    </Col>
+    <div className="join-squad-child">
+      <p>
+        Hi, I'm {child.ChildName}!
+        <br />
+        Nice to meet you!
+      </p>
+      <img
+        className={`word-bubble-${childNum}`}
+        src={bubbleStyle === 'right' ? wordBubbleright : wordBubble}
+        alt="word bubble"
+      />
+      <img
+        className={`star-bg-${childNum}`}
+        src={Squadup}
+        alt="blast character background"
+      />
+      <img
+        className={`child-avatar-${childNum}`}
+        src={child.AvatarURL}
+        alt="child avatar"
+      />
+    </div>
   );
 };
 export default RenderChild;
