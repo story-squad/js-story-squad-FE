7fa252fb8f9440ef6ce0cc062ef38d9e4d5e4fa3
Drew <mail.drew.ross@gmail.com>
2021-04-30 12:08:21 -0700

    add compact view logic

--- src/components/common/SubmissionViewer.js
+++ src/components/common/SubmissionViewer.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import zoomIcon from '../../assets/icons/zoom-icon.svg';
+import { toCapitalized } from '../../utils/helpers';
 
-const SubmissionViewer = ({ src }) => {
+const SubmissionViewer = ({ src, type, compact = false }) => {
   const [showModal, setShowModal] = useState(true);
 
   return (
-    <button className="submission-viewer" onClick={() => setShowModal(true)}>
+    <button
+      className={`submission-viewer ${compact ? 'compact' : ''}`}
+      onClick={() => setShowModal(true)}
+    >
       <div
         className="submission-img"
         style={{ backgroundImage: `url(${src})` }}
       ></div>
       <div className="magnify-icon bg-aqua">
         <img src={zoomIcon} alt="magnifying glass icon" />
+        {compact && <p>{toCapitalized(type)}</p>}
       </div>
     </button>
   );
