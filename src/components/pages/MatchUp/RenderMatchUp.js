cdb687baac0d96380f1815495ca3d5e4bae096ef
Drew <mail.drew.ross@gmail.com>
2021-05-03 12:15:06 -0700

    add team match up content box

--- src/components/pages/MatchUp/RenderMatchUp.js
+++ src/components/pages/MatchUp/RenderMatchUp.js
@@ -4,19 +4,24 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
 
 import FaceoffContent from './FaceoffContent';
-import { InstructionsModal } from '../../common';
-import { modalInstructions } from '../../../utils/helpers';
+import { ChildAvatar, InstructionsModal } from '../../common';
+import {
+  modalInstructions,
+  getTeamsFromFaceoffs,
+} from '../../../utils/helpers';
 
 const RenderMatchUp = props => {
   const { push } = useHistory();
   const [faceoffs, setFaceoffs] = useState([]);
   const [modalVisible, setModalVisible] = useState(true);
+  const [teams, setTeams] = useState({});
 
   useEffect(() => {
     if (props.child.VotesRemaining <= 9) {
       setModalVisible(false);
     }
     setFaceoffs(props.faceoffs);
+    setTeams(getTeamsFromFaceoffs(props.faceoffs));
   }, [props]);
   const handleVote = e => {
     e.preventDefault();
@@ -29,12 +34,12 @@ const RenderMatchUp = props => {
 
   return (
     <>
-      <QuestionCircleOutlined
+      {/* <QuestionCircleOutlined
         className="question-icon"
         onClick={() => {
           setModalVisible(true);
         }}
-      />
+      /> */}
       {modalVisible && (
         <InstructionsModal
           modalVisible={modalVisible}
@@ -48,6 +53,37 @@ const RenderMatchUp = props => {
         />
       )}
       <div className="matchup-container">
+        <div className="content-box dark shaped center-content">
+          <h2>The Match Up</h2>
+          <p>201 points to win</p>
+          <div className="flex space-between align-center margin-bottom-2">
+            {faceoffs.length === 4 && (
+              <>
+                <ChildAvatar
+                  src={teams[1][0].AvatarURL}
+                  name={teams[1][0].Name}
+                  fontColor={'light'}
+                />
+                <ChildAvatar
+                  src={teams[1][1].AvatarURL}
+                  name={teams[1][1].Name}
+                  fontColor={'light'}
+                />
+                <p className="h2 text-light">VS</p>
+                <ChildAvatar
+                  src={teams[2][0].AvatarURL}
+                  name={teams[2][0].Name}
+                  fontColor={'light'}
+                />
+                <ChildAvatar
+                  src={teams[2][1].AvatarURL}
+                  name={teams[2][1].Name}
+                  fontColor={'light'}
+                />
+              </>
+            )}
+          </div>
+        </div>
         {faceoffs.length === 4 && (
           <div className="grid grid-2-col">
             <FaceoffContent
