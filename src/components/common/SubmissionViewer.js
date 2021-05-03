6d4e09695d206db2e84270f829a056d6e8bf6ed3
Drew <mail.drew.ross@gmail.com>
2021-05-03 13:23:34 -0700

    add conditional view for 'vote to unlock' button, update JSX heirarcy, classNames for styling

--- src/components/common/SubmissionViewer.js
+++ src/components/common/SubmissionViewer.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react';
 import zoomIcon from '../../assets/icons/zoom-icon.svg';
+import lockIcon from '../../assets/icons/lock.svg';
 import { toCapitalized } from '../../utils/helpers';
 
-const SubmissionViewer = ({ src, type, compact = false }) => {
+const SubmissionViewer = ({
+  src,
+  type,
+  compact = false,
+  locked = false,
+  canVote = false,
+}) => {
   const [showModal, setShowModal] = useState(true);
 
+  const handleOnClick = () => {
+    if (!locked) {
+      setShowModal(true);
+    }
+  };
+
   return (
-    <button
-      className={`submission-viewer ${compact ? 'compact' : ''}`}
-      onClick={() => setShowModal(true)}
-    >
-      <div
-        className="submission-img"
-        style={{ backgroundImage: `url(${src})` }}
-      ></div>
-      <div className="magnify-icon bg-aqua">
-        <img src={zoomIcon} alt="magnifying glass icon" />
-        {compact && <p>{toCapitalized(type)}</p>}
-      </div>
-    </button>
+    <div className="submission-viewer-button-container">
+      {locked && canVote && (
+        <button className="submission-viewer-vote">
+          <img className="vote-lock-icon" src={lockIcon} alt="lock icon" />
+          <span>Vote to unlock</span>
+        </button>
+      )}
+      <button
+        className={`submission-viewer-button ${compact ? 'compact' : ''}`}
+        onClick={handleOnClick}
+        disabled={locked}
+      >
+        <div
+          className="submission-img"
+          style={{ backgroundImage: `url(${src})` }}
+        ></div>
+        <div className="magnify-icon bg-aqua">
+          <img src={zoomIcon} alt="magnifying glass icon" />
+          {compact && <p>{toCapitalized(type)}</p>}
+        </div>
+      </button>
+    </div>
   );
 };
 
