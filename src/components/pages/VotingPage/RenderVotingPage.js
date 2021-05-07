44a18cb9f4ade98e283ae9ad1f7c9267e6219279
Drew <mail.drew.ross@gmail.com>
2021-05-07 11:15:26 -0700

    add submission type text and numbers

--- src/components/pages/VotingPage/RenderVotingPage.js
+++ src/components/pages/VotingPage/RenderVotingPage.js
@@ -7,6 +7,7 @@ const RenderVotingPage = props => {
 
   return (
     <div className="voting-container">
+      {console.log(props)}
       <div className="shaped-shadow-container">
         <div className="content-box dark shaped center-content">
           <h2>Vote for Your Favorite Story</h2>
@@ -19,9 +20,11 @@ const RenderVotingPage = props => {
       </div>
       <div className="submissions grid grid-2-col">
         <div className="submission content-box center-content dark">
+          <p className="font-display">{props.votes.Type} 1</p>
           <SubmissionViewer src={props.votes.Submission1.ImgURL} />
         </div>
         <div className="submission content-box center-content dark">
+          <p className="font-display">{props.votes.Type} 2</p>
           <SubmissionViewer src={props.votes.Submission2.ImgURL} />
         </div>
       </div>
