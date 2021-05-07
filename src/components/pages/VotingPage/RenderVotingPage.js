81533df9fdb425f10a1386fc816abe473dbfde53
Drew <mail.drew.ross@gmail.com>
2021-05-07 10:59:32 -0700

    update JSX heirarchy, div classNames, replace img tags with SubmissionViewer components, comment-out EmojiPicker components

--- src/components/pages/VotingPage/RenderVotingPage.js
+++ src/components/pages/VotingPage/RenderVotingPage.js
@@ -1,45 +1,28 @@
 import React, { useState } from 'react';
-import { VotingForm, EmojiPicker } from '../../common';
-import { Row, Col } from 'antd';
+import { VotingForm, EmojiPicker, SubmissionViewer } from '../../common';
 
 const RenderVotingPage = props => {
   const [subEmojis1, setSubEmojis1] = useState('');
   const [subEmojis2, setSubEmojis2] = useState('');
 
   return (
-    <>
-      <div className="voting-container">
-        <Row className="main-row">
-          <Col className="left-half" xs={24} sm={12}>
-            <div className="image-and-check-container">
-              <img
-                className="WritingandDrawingIcon"
-                // ERRLOG: submission1 instead of submissionID1(??)
-                src={props.votes.Submission1.ImgURL}
-                alt="writing/drawing submission"
-              />
-              <EmojiPicker getChildState={setSubEmojis1} />
-            </div>
-          </Col>
-          <Col className="right-half" xs={24} sm={12}>
-            <div className="image-and-check-container">
-              <img
-                className="WritingandDrawingIcon"
-                // ERRLOG: submission2 instead of submissionID2(??)
-                src={props.votes.Submission2.ImgURL}
-                alt="writing/drawing submission"
-              />
-            </div>
-            <EmojiPicker getChildState={setSubEmojis2} />
-            <VotingForm
-              faceoffToVote={props.votes}
-              child={props.child}
-              subEmojis={{ subEmojis1, subEmojis2 }}
-            />
-          </Col>
-        </Row>
+    <div className="voting-container">
+      <div className="submissions">
+        <div className="submission content-box">
+          <SubmissionViewer src={props.votes.Submission1.ImgURL} />
+        </div>
+        <div className="submission content-box">
+          <SubmissionViewer src={props.votes.Submission2.ImgURL} />
+        </div>
       </div>
-    </>
+      <VotingForm
+        faceoffToVote={props.votes}
+        child={props.child}
+        subEmojis={{ subEmojis1, subEmojis2 }}
+      />
+      {/* <EmojiPicker getChildState={setSubEmojis1} />
+      <EmojiPicker getChildState={setSubEmojis2} /> */}
+    </div>
   );
 };
 export default RenderVotingPage;
