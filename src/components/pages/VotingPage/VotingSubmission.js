2ae10cc13c064fbd68bda79850dece2d71d1a663
Drew <mail.drew.ross@gmail.com>
2021-05-10 11:13:27 -0700

    add VotingSubmission component

---
+++ src/components/pages/VotingPage/VotingSubmission.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { SubmissionViewer } from '../../common';
+
+const VotingSubmission = ({
+  submissionId,
+  selectedValue,
+  setSelectedValue,
+  imgSrc,
+  submissionType,
+}) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  useEffect(() => {
+    setIsSelected(submissionId === selectedValue);
+  }, [submissionId, selectedValue]);
+
+  return (
+    <div
+      className={`submission content-box center-content dark ${
+        isSelected && 'selected'
+      }`}
+    >
+      <p className="font-display">{submissionType} 2</p>
+      <SubmissionViewer src={imgSrc} />
+      <button
+        className={`secondary text-dark ${isSelected && 'font-display'}`}
+        onClick={() => setSelectedValue(submissionId)}
+      >
+        {isSelected ? '✓ Selected' : 'Select'}
+      </button>
+    </div>
+  );
+};
+
+export default VotingSubmission;
