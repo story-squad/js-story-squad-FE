74087d657ece6b0683e8adeb873ad1651e610899
Drew <mail.drew.ross@gmail.com>
2021-05-10 12:13:41 -0700

    clean up unused functions, implement submit vote function/button, remove commented-out code

--- src/components/pages/VotingPage/VotingForm.js
+++ src/components/pages/VotingPage/VotingForm.js
@@ -10,12 +10,8 @@ const VotingForm = props => {
   const { authState } = useOktaAuth();
   const { subEmojis1, subEmojis2 } = props.subEmojis;
   const [value, setValue] = useState();
-  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-  const onChange = e => {
-    setValue(e.target.value);
-  };
-  const onFinish = () => {
+  const handleSubmit = () => {
     const body = {
       Vote: value,
       MemberID: props.child.memberId,
@@ -38,60 +34,35 @@ const VotingForm = props => {
       push('/child/match-up');
     });
   };
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-
-  const onValuesChange = () => {
-    setButtonDisabled(false);
-  };
 
   return (
-    // <Form
-    //   onValuesChange={onValuesChange}
-    //   name="basic"
-    //   initialValues={{ remember: true }}
-    //   onFinish={onFinish}
-    //   onFinishFailed={onFinishFailed}
-    // >
-    //   <Form.Item name="vote" valuePropName="checked">
-    //     <Radio.Group onChange={onChange} value={value}>
-    //       <Radio
-    //         className="left-radio"
-    //         value={props.faceoffToVote.SubmissionID1}
-    //       />
-    //       <Radio
-    //         className="right-radio"
-    //         value={props.faceoffToVote.SubmissionID2}
-    //       />
-    //     </Radio.Group>
-    //   </Form.Item>
-    //   <Form.Item>
-    //     <Button
-    //       className="votingSubmit-button"
-    //       htmlType="submit"
-    //       disabled={buttonDisabled}
-    //     >
-    //       Submit
-    //     </Button>
-    //   </Form.Item>
-    // </Form>
-    <div className="submissions grid grid-2-col grid-small">
-      <VotingSubmission
-        submissionId={props.faceoffToVote.SubmissionID1}
-        selectedValue={value}
-        setSelectedValue={setValue}
-        imgSrc={props.faceoffToVote.Submission1.ImgURL}
-        submissionType={props.faceoffToVote.Type}
-      />
-      <VotingSubmission
-        submissionId={props.faceoffToVote.SubmissionID2}
-        selectedValue={value}
-        setSelectedValue={setValue}
-        imgSrc={props.faceoffToVote.Submission2.ImgURL}
-        submissionType={props.faceoffToVote.Type}
-      />
-    </div>
+    <>
+      <div className="submissions grid grid-2-col grid-small">
+        <VotingSubmission
+          submissionId={props.faceoffToVote.SubmissionID1}
+          selectedValue={value}
+          setSelectedValue={setValue}
+          imgSrc={props.faceoffToVote.Submission1.ImgURL}
+          submissionType={props.faceoffToVote.Type}
+        />
+        <VotingSubmission
+          submissionId={props.faceoffToVote.SubmissionID2}
+          selectedValue={value}
+          setSelectedValue={setValue}
+          imgSrc={props.faceoffToVote.Submission2.ImgURL}
+          submissionType={props.faceoffToVote.Type}
+        />
+      </div>
+      <div className="center-content margin-bottom-4">
+        <button
+          onClick={handleSubmit}
+          className="min-width"
+          disabled={value === undefined}
+        >
+          Vote
+        </button>
+      </div>
+    </>
   );
 };
 
