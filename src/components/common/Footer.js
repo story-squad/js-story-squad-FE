2ea665b6c95009420a2639c114f64bd2bcbc9a18
Drew <mail.drew.ross@gmail.com>
2021-05-14 12:52:11 -0700

    add Footer content

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="content">
+        <p>&copy;2021 Story Squad HQ</p>
+        <nav>
+          <ul>
+            <li>
+              <a>Contact</a>
+            </li>
+            <li>
+              <a>FAQs</a>
+            </li>
+            <li>
+              <a>Privacy</a>
+            </li>
+            <li>
+              <a>Terms</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
