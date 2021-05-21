0ad6b57f547abe40422d25948b7634c4a7b6f2e9
Drew <mail.drew.ross@gmail.com>
2021-05-21 13:11:03 -0700

    add EmojiPicker, add child elements to SubmissionViewer

--- src/components/pages/VotingPage/VotingSubmission.js
+++ src/components/pages/VotingPage/VotingSubmission.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { SubmissionViewer } from '../../common';
+import { EmojiPicker, SubmissionViewer } from '../../common';
 import thumbIcon from '../../../assets/icons/thumbsup.svg';
+import { emojiLimit } from '../../../utils/constants';
 
 const VotingSubmission = ({
   number,
@@ -9,6 +10,7 @@ const VotingSubmission = ({
   setSelectedValue,
   imgSrc,
   submissionType,
+  setEmojis,
 }) => {
   const [isSelected, setIsSelected] = useState(false);
 
@@ -25,7 +27,13 @@ const VotingSubmission = ({
       <p className="margin-bottom-2 font-display">
         {submissionType} {number}
       </p>
-      <SubmissionViewer src={imgSrc} />
+      <SubmissionViewer src={imgSrc} modalButtonText="Back to Voting">
+        <p>
+          Choose up to {emojiLimit} different emojis to express how you feel
+          about this story.
+        </p>
+        <EmojiPicker getChildState={setEmojis} />
+      </SubmissionViewer>
       <div className="select-container">
         {isSelected ? (
           <div className="center-content-flex">
