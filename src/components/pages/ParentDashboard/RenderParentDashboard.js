d55d7b6aa477883233be536f613c2746ef97a8e2
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-28 17:58:22 -0700

    placeholder for children progress in parent dash brought over

--- src/components/pages/ParentDashboard/RenderParentDashboard.js
+++ src/components/pages/ParentDashboard/RenderParentDashboard.js
@@ -9,38 +9,7 @@ import ParentNavSider from '../../common/ParentNavSider';
 const { Title } = Typography;
 
 const ParentDashboard = props => {
-  return (
-    <>
-      <Layout className="parent-dashboard">
-        <ParentNavSider selected="dashboard" />
-        <Layout>
-          <Title className="title" style={{ color: '#0267C1' }} level={1}>
-            STORY SQUAD
-          </Title>
-          <div className="children-container">
-            <Layout className="children" style={{ flexFlow: 'row wrap' }}>
-              {props.parent.children.map((child, i) => (
-                <ChildCard
-                  key={child.ID}
-                  id={child.ID}
-                  name={child.Name}
-                  AvatarURL={child.AvatarURL}
-                  update="PROGRESS"
-                />
-              ))}
-              <Card>
-                <h2>
-                  <Link to="/parent/add-child">
-                    <PlusCircleFilled /> Add a Child
-                  </Link>
-                </h2>
-              </Card>
-            </Layout>
-          </div>
-        </Layout>
-      </Layout>
-    </>
-  );
+  return <></>;
 };
 
 export default ParentDashboard;
