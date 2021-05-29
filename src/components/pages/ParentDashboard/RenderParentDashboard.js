5d7fa6840b9fb7c8656990a7410e6b451a0d8cdf
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-28 18:25:25 -0700

    completed pulling code over for placeholder

--- src/components/pages/ParentDashboard/RenderParentDashboard.js
+++ src/components/pages/ParentDashboard/RenderParentDashboard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Layout, Typography, Card } from 'antd';
 import { Link } from 'react-router-dom';
-
+import ParentDashPlaceholder from '../../common/ParentDashPlaceholder';
 import { PlusCircleFilled } from '@ant-design/icons';
-import ChildCard from '../../common/ChildCard';
-import ParentNavSider from '../../common/ParentNavSider';
 
 const { Title } = Typography;
 
 const ParentDashboard = props => {
-  return <></>;
+  return (
+    <>
+      <Layout className="newparent-dashboard">
+        <ParentDashPlaceholder />
+      </Layout>
+    </>
+  );
 };
 
 export default ParentDashboard;
