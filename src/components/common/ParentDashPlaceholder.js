4291928b2d5856758b6401414532fc907b1c846d
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-28 18:25:25 -0700

    completed pulling code over for placeholder

--- src/components/common/ParentDashPlaceholder.js
+++ src/components/common/ParentDashPlaceholder.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 export default function NewProgressCharts(props) {
   return (
-    <div className="ProgressContainer">
-      <div className="ProgressHeader">
-        <h2>Progress Charts</h2>
-      </div>
-      <div className="ProgressBoxContainer">
-        <div className="ProgressBox">
-          <br />
-          <h4>Progress Chart will be displayed here!</h4>
-          <br />
-          <br />
-        </div>
+    <div
+      className="content-box dark"
+      style={{ backgroundColor: '#E5E5E5', borderRadius: '25px' }}
+    >
+      <div className="ProgressBox">
+        <h3>This is a placeholder</h3>
+        <h3>Progress Chart will be displayed here!</h3>
       </div>
     </div>
   );
