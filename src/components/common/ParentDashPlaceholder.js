302eb91cf830d1a0bbba769d8f6fb44f3268d0b8
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-05-31 10:54:17 -0700

    completed emailPass dummy form inputs

--- src/components/common/ParentDashPlaceholder.js
+++ src/components/common/ParentDashPlaceholder.js
@@ -6,7 +6,7 @@ export default function NewProgressCharts(props) {
       className="content-box dark"
       style={{ backgroundColor: '#E5E5E5', borderRadius: '25px' }}
     >
-      <div className="ProgressBox">
+      <div className="ProgressBox parent-styles">
         <h3>This is a placeholder</h3>
         <h3>Progress Chart will be displayed here!</h3>
       </div>
