0d69f5a5b5a0bce42b7057a84e73a4661aebdf19
Trevor Martin <trevor.j.martin@gmail.com>
2021-06-13 19:47:16 -0400

    todo, fix profile link

--- src/components/pages/ProfileSelect/ProfileSelectContainer.js
+++ src/components/pages/ProfileSelect/ProfileSelectContainer.js
@@ -10,26 +10,27 @@ function ProfileSelectContainer({ LoadingComponent, ...props }) {
   const { authState, authService } = useOktaAuth();
   const [userInfo, setUserInfo] = useState(null);
   // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [authService], []);
-
+  const [memoAuthState] = useMemo(() => [authState], []);
   useEffect(() => {
-    let isSubscribed = true;
-
-    memoAuthService
-      .getUser()
-      .then(info => {
-        // if user is authenticated we can use the authService to snag some user info.
-        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isSubscribed) {
-          setUserInfo(info);
-        }
-      })
-      .catch(err => {
-        isSubscribed = false;
-        return setUserInfo(null);
-      });
-    return () => (isSubscribed = false);
-  }, [memoAuthService]);
+    if (memoAuthState?.isAuthenticated) {
+      // if the user is authentic, get their profile info
+      return setUserInfo(memoAuthState);
+    }
+    // memoAuthService
+    //   .getUser()
+    //   .then(info => {
+    //     // if user is authenticated we can use the authService to snag some user info.
+    //     // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+    //     if (isSubscribed) {
+    //       setUserInfo(info);
+    //     }
+    //   })
+    //   .catch(err => {
+    //     isSubscribed = false;
+    //     return setUserInfo(null);
+    //   });
+    return () => false;
+  }, [memoAuthState]);
 
   return (
     <>
