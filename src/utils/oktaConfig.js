f9d629a52ffa6384226caa8c30360c349f2fed51
Trevor Martin <trevor.j.martin@gmail.com>
2021-06-14 18:57:46 -0400

    new oktaConfig

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,6 +1,6 @@
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
-let USE_INTERACTION_CODE = false;
+// let USE_INTERACTION_CODE = false;
 
 const config = {
   issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
@@ -9,15 +9,14 @@ const config = {
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
   disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
-  useInteractionCode: USE_INTERACTION_CODE,
+  // useInteractionCode: USE_INTERACTION_CODE,
 };
-
-const oktaAuth = new OktaAuth(config);
 
 function genRestore(_history) {
   return async (_oktaAuth, originalUri) => {
     _history.replace(toRelativeUrl(originalUri, window.location.origin));
   };
 }
+const oktaAuth = new OktaAuth(config);
 
-export { config, oktaAuth, genRestore };
+export { oktaAuth, config, genRestore };
