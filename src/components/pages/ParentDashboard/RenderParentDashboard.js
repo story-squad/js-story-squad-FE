6c4978b69ce9e433e5f723d12015e9e07fe091b2
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-06-25 10:58:16 -0700

    finished changing styling over from parent dash from scss to less

--- src/components/pages/ParentDashboard/RenderParentDashboard.js
+++ src/components/pages/ParentDashboard/RenderParentDashboard.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import { Layout, Typography, Card } from 'antd';
-import { Link } from 'react-router-dom';
-import ParentDashPlaceholder from '../../common/ParentDashPlaceholder';
-import { PlusCircleFilled } from '@ant-design/icons';
-import ChildCard from '../../common/ChildCard';
-import { AccountSettings } from '../ParentAccountSettings';
+import React, { useEffect } from 'react';
+import { Layout } from 'antd';
+import { useOktaAuth } from '@okta/okta-react';
+import { getProfileData } from '../../../api';
+import ParentNavTopBar from '../../common/ParentNavTop';
+// import NewProgressCharts from '../../common/ProgressChart';
+import NewChildCard from '../../common/ChildCard';
+import AccountSettings from '../ParentAccountSettings/AccountSettingsContainer';
+import { connect } from 'react-redux';
+import { setParent } from '../../../state/actions/parentActions';
 
-const { Title } = Typography;
+const RenderNewParentDashboard = props => {
+  const { authState } = useOktaAuth();
+  const { setParent } = props;
 
-const ParentDashboard = props => {
+  useEffect(() => {
+    getProfileData(authState).then(res => {
+      setParent({
+        ...res[0],
+        children: res.filter(user => user.type !== 'Parent'),
+      });
+    });
+  }, [setParent, authState]);
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <ParentDashPlaceholder />
+    <div>
+      <Layout className="newparent-dashboard">
+        <ParentNavTopBar />
+        <Layout>
+          <div className="ProgressContainer">
+            <div className="ProgressHeader">
+              <h2>Progress Charts</h2>
+            </div>
+            <div className="ProgressBoxContainer">
+              <div className="ProgressBox">
+                {/* <NewProgressCharts /> */}
+                <br />
+                <h4>Progress Chart will be displayed here!</h4>
+                <br />
+                <br />
+              </div>
+            </div>
+          </div>
 
-        <ChildCard />
-        <br />
-        <AccountSettings />
-      </div>
-    </>
+          <div className="child-container">
+            <NewChildCard props={props} />
+          </div>
+          <div>
+            <AccountSettings />
+          </div>
+        </Layout>
+      </Layout>
+    </div>
   );
 };
 
-export default ParentDashboard;
+export default connect(null, { setParent: setParent })(
+  RenderNewParentDashboard
+);
