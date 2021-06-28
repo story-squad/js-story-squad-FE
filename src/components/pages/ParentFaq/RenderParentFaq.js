9c9b24d964c031060575f8b0938dac4985431c15
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-06-28 15:22:46 -0700

    minor changes to wording of questions made

--- src/components/pages/ParentFaq/RenderParentFaq.js
+++ src/components/pages/ParentFaq/RenderParentFaq.js
@@ -65,7 +65,7 @@ function RenderParentFaq(props) {
               )}
             >
               <Panel
-                header="Q-1. STAR: Is Story Squad safe and appropriate for my children?"
+                header="Is Story Squad safe and appropriate for my children?"
                 key="1"
                 className="ant-card-body"
               >
@@ -79,7 +79,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-2. What type of content is available?"
+                header="What type of content is available?"
                 key="2"
                 className="ant-card-body"
               >
@@ -93,7 +93,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-3. What's the weekly schedule for the game?"
+                header="What's the weekly schedule for the game?"
                 key="3"
                 className="ant-card-body"
               >
@@ -107,7 +107,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-4. What happen if a phase ends and my child forgot to participate
+                header="What happen if a phase ends and my child forgot to participate
                 in time?"
                 key="4"
                 className="ant-card-body"
@@ -122,7 +122,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-5. How are partners assigned and opposing teams selected?"
+                header="How are partners assigned and opposing teams selected?"
                 key="5"
                 className="ant-card-body"
               >
@@ -136,7 +136,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-6. What is Story Squad privacy policy?"
+                header="What is Story Squad privacy policy?"
                 key="6"
                 className="ant-card-body"
               >
@@ -150,7 +150,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-7. How does the gallery and leaderboard work?"
+                header="How does the gallery and leaderboard work?"
                 key="7"
                 className="ant-card-body"
               >
@@ -164,7 +164,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-8. I'm a child's book author. How do I get my book on Story Squad?"
+                header="I'm a child's book author. How do I get my book on Story Squad?"
                 key="8"
                 className="ant-card-body"
               >
@@ -195,7 +195,7 @@ function RenderParentFaq(props) {
               )}
             >
               <Panel
-                header="Q-1. How do I add/edit my child's profile PIN?"
+                header="How do I add/edit my child's profile PIN?"
                 key="1"
                 className="ant-card-body"
               >
@@ -209,7 +209,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-2. How do I create my child's profile?"
+                header="How do I create my child's profile?"
                 key="2"
                 className="ant-card-body"
               >
@@ -223,7 +223,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-3. How do I change my child's avatar?"
+                header="How do I change my child's avatar?"
                 key="3"
                 className="ant-card-body"
               >
@@ -237,7 +237,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-4. How do I change my child's profile name?"
+                header="How do I change my child's profile name?"
                 key="4"
                 className="ant-card-body"
               >
@@ -251,7 +251,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-5. How do I delete my child's profile?"
+                header="How do I delete my child's profile?"
                 key="5"
                 className="ant-card-body"
               >
@@ -265,7 +265,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-6. How do I receive updates on my child's progress?"
+                header="How do I receive updates on my child's progress?"
                 key="6"
                 className="ant-card-body"
               >
@@ -296,7 +296,7 @@ function RenderParentFaq(props) {
               )}
             >
               <Panel
-                header="Q-1. STAR: I forget my password. What should I do?"
+                header="I forget my password. What should I do?"
                 key="1"
                 className="ant-card-body"
               >
@@ -310,7 +310,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-2. How do I update my parent email for my Story Squad
+                header="How do I update my parent email for my Story Squad
                 subscription?"
                 key="2"
                 className="ant-card-body"
@@ -342,7 +342,7 @@ function RenderParentFaq(props) {
               )}
             >
               <Panel
-                header="Q-1. STAR: I forgot my password. What should I do?"
+                header="I forgot my password. What should I do?"
                 key="1"
                 className="ant-card-body"
               >
@@ -356,7 +356,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-2. The Story Squard app isn't responding. What should I do?"
+                header="The Story Squard app isn't responding. What should I do?"
                 key="2"
                 className="ant-card-body"
               >
@@ -370,7 +370,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-3. What devices does Story Squard support?"
+                header="What devices does Story Squard support?"
                 key="3"
                 className="ant-card-body"
               >
@@ -384,7 +384,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-4. Is an internet connection required to use Story Squard?"
+                header="Is an internet connection required to use Story Squard?"
                 key="4"
                 className="ant-card-body"
               >
@@ -398,7 +398,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-5. How do I add multiple devices?"
+                header="How do I add multiple devices?"
                 key="5"
                 className="ant-card-body"
               >
@@ -412,7 +412,7 @@ function RenderParentFaq(props) {
                 </p>
               </Panel>
               <Panel
-                header="Q-6. Will my children's progress sync across all devices registered
+                header="Will my children's progress sync across all devices registered
                 to their account?"
                 key="6"
                 className="ant-card-body"
